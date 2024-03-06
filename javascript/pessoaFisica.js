0fcbd6141452dc6b66f6a162050f6af648a3d4be
Monalisa017 <61711279+Monalisa017@users.noreply.github.com>
2024-03-06 10:27:21 -0300

    feat: Registrando usuário

--- javascript/pessoaFisica.js
+++ javascript/pessoaFisica.js
@@ -20,3 +20,77 @@ document.getElementById('cpf').addEventListener('input', function (e) {
     valor = valor.replace(/(\d{3})(\d{1,2})$/, '$1-$2'); // Insere hífen antes dos dois últimos dígitos
     e.target.value = valor;
 });
+
+
+// Simula um "Banco de Dados" com localStorage
+class UserDB {
+    constructor() {
+        this.key = 'users';
+    }
+
+    // Cria ou adiciona um novo usuário
+    createUser(user) {
+        let users = JSON.parse(localStorage.getItem(this.key)) || [];
+        users.push(user);
+        localStorage.setItem(this.key, JSON.stringify(users));
+    }
+
+    // Lê todos os usuários
+    readUsers() {
+        return JSON.parse(localStorage.getItem(this.key)) || [];
+    }
+
+    // Atualiza um usuário pelo ID
+    updateUser(id, newUserDetails) {
+        let users = this.readUsers();
+        let userIndex = users.findIndex(user => user.id === id);
+        if (userIndex !== -1) {
+            users[userIndex] = { ...users[userIndex], ...newUserDetails };
+            localStorage.setItem(this.key, JSON.stringify(users));
+        }
+    }
+
+    // Deleta um usuário pelo ID
+    deleteUser(id) {
+        let users = this.readUsers();
+        users = users.filter(user => user.id !== id);
+        localStorage.setItem(this.key, JSON.stringify(users));
+    }
+}
+
+// Exemplo de uso
+
+document.getElementById("btnEnviar").onclick = function enviarRegistro() {
+    console.log("Entrando");
+    let db = new UserDB();
+    let lastId = 1;
+
+    function generateNextId() {
+        return ++lastId;
+    }
+    const nome = document.getElementById("name").value;
+    const dataNascimento = document.getElementById("dataNascimento").value;
+    const rg = document.getElementById("rg").value;
+    const cpf = document.getElementById("cpf").value;
+    const email = document.getElementById("email").value;
+    const telefone = document.getElementById("telefone").value;
+
+    const usuario = { id: generateNextId(), nome: nome, dataNascimento: dataNascimento, rg: rg, cpf: cpf, email: email, telefone: telefone }
+    // Criar usuários
+    db.createUser(usuario);
+
+    // Ler usuários
+    console.log(db.readUsers());
+};
+
+let db = new UserDB();
+
+// Atualizar usuário
+db.updateUser(1, { nome: 'João Atualizado', email: 'joaoatualizado@example.com' });
+
+// Deletar usuário
+db.deleteUser(2);
+
+// Ler usuários após atualização e deleção
+console.log(db.readUsers());
+
